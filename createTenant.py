654c5f5dadfb26e52b5db106f22633176ce5b674
root <root@k8s-115-master.tme-lab.local>
2021-04-23 15:49:58 +0200

    create hundreds of tenants

--- createTenant.py
+++ createTenant.py
@@ -11,13 +11,31 @@ try:
 except ImportError:
     sys.exit("Error: please verify credentials file format.")
 
+tenantId = []
 tenantJson = '''
-{"displayName":"abc","name":"abc","description":"fred","siteAssociations":[{"siteId":"6081c2155fb41db238e1c69a","securityDomains":[]}],"userAssociations":[]}
+{"displayName":"%s",
+ "name":"%s",
+ "description":"%s",
+ "siteAssociations":[
+   {"siteId":"6081c2155fb41db238e1c69a",
+    "securityDomains":[]
+   }],
+ "userAssociations":[]}
 '''
 
 rc = mso.RestClient(MSO_IP, MSO_ADMIN, MSO_PASSWORD, api_version="v1")
 
-resp = rc.post('/tenants', json_body=json.loads(tenantJson))
-allNodes = json.loads(resp.text)
-pprint.pprint(allNodes)
+for i in range(100):
+    tenant = tenantJson % ("DemoB-"+str(i),"demoB-"+str(i),"API test "+str(i))
+    resp = rc.post('/tenants', json_body=json.loads(tenant))
+    allNodes = json.loads(resp.text)
+    pprint.pprint(allNodes)
+    tenantId.append(allNodes['id'])
+    
+print("Check the UI. I will now delete all tenants")
+temp = raw_input("continue? ")
 
+for i in range(len(tenantId)):
+    delUrl = '/tenants/' + tenantId[i]
+    resp = rc.delete(delUrl)
+    print("deleted {}".format(i))
