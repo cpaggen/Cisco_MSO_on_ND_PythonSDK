41369f3dfc146316be51fb29d096e3ad91f7eed8
Chris Paggen <cpaggen@gmail.com>
2021-09-09 13:41:35 +0200

    static path PUT

--- runDemo.py
+++ runDemo.py
@@ -216,10 +216,149 @@ siteData = '''
 }
 '''
 
+staticPathBinding = '''
+{
+    "id": "613927591f0000080a98c007",
+    "displayName": "testSchema",
+    "description": "",
+    "templates": [
+        {
+            "name": "testTemplate",
+            "displayName": "testTemplate",
+            "tenantId": "6138f3ef1f0000520398bf81",
+            "anps": [
+                {
+                    "name": "ap1",
+                    "displayName": "ap1",
+                    "anpRef": "/schemas/613927591f0000080a98c007/templates/testTemplate/anps/ap1",
+                    "epgs": [
+                        {
+                            "name": "epg1",
+                            "displayName": "epg1",
+                            "epgRef": "/schemas/613927591f0000080a98c007/templates/testTemplate/anps/ap1/epgs/epg1",
+                            "contractRelationships": [],
+                            "subnets": [],
+                            "uSegEpg": false,
+                            "uSegAttrs": [],
+                            "intraEpg": "unenforced",
+                            "prio": "unspecified",
+                            "proxyArp": false,
+                            "mCastSource": false,
+                            "preferredGroup": false,
+                            "bdRef": "/schemas/613927591f0000080a98c007/templates/testTemplate/bds/bd1",
+                            "vrfRef": "",
+                            "selectors": [],
+                            "epgType": "application"
+                        }
+                    ]
+                }
+            ],
+            "vrfs": [
+                {
+                    "name": "vrf1",
+                    "displayName": "vrf1",
+                    "vrfRef": "/schemas/613927591f0000080a98c007/templates/testTemplate/vrfs/vrf1",
+                    "l3MCast": false,
+                    "preferredGroup": false,
+                    "vzAnyEnabled": false,
+                    "vzAnyProviderContracts": [],
+                    "vzAnyConsumerContracts": [],
+                    "rpConfigs": [],
+                    "pcEnfPref": "enforced",
+                    "ipDataPlaneLearning": "enabled",
+                    "vrfDescr": ""
+                }
+            ],
+            "bds": [
+                {
+                    "name": "bd1",
+                    "displayName": "bd1",
+                    "bdRef": "/schemas/613927591f0000080a98c007/templates/testTemplate/bds/bd1",
+                    "l2UnknownUnicast": "proxy",
+                    "intersiteBumTrafficAllow": false,
+                    "optimizeWanBandwidth": false,
+                    "l2Stretch": false,
+                    "l3MCast": false,
+                    "subnets": [],
+                    "vrfRef": "/schemas/613927591f0000080a98c007/templates/testTemplate/vrfs/vrf1",
+                    "unkMcastAct": "flood",
+                    "v6unkMcastAct": "flood",
+                    "arpFlood": true,
+                    "multiDstPktAct": "bd-flood",
+                    "dhcpLabels": [],
+                    "unicastRouting": true
+                }
+            ],
+            "contracts": [],
+            "filters": [],
+            "externalEpgs": [],
+            "serviceGraphs": [],
+            "intersiteL3outs": [],
+            "templateType": "stretched-template",
+            "templateSubType": [],
+            "networks": [],
+            "version": 3
+        }
+    ],
+    "sites": [
+        {
+            "siteId": "6138cf80ca038ce79fe968e6",
+            "templateName": "testTemplate",
+            "anps": [
+                {
+                    "anpRef": "/schemas/613927591f0000080a98c007/templates/testTemplate/anps/ap1",
+                    "epgs": [
+                        {
+                            "epgRef": "/schemas/613927591f0000080a98c007/templates/testTemplate/anps/ap1/epgs/epg1",
+                            "domainAssociations": [],
+                            "staticPorts": [
+                                {
+                                    "type": "port",
+                                    "path": "topology/pod-1/paths-101/pathep-[eth1/6]",
+                                    "portEncapVlan": 666,
+                                    "deploymentImmediacy": "lazy",
+                                    "mode": "regular"
+                                }
+                            ],
+                            "staticLeafs": [],
+                            "uSegAttrs": [],
+                            "subnets": [],
+                            "selectors": []
+                        }
+                    ]
+                }
+            ],
+            "vrfs": [],
+            "bds": [
+                {
+                    "bdRef": "/schemas/613927591f0000080a98c007/templates/testTemplate/bds/bd1",
+                    "subnets": [],
+                    "l3Outs": [],
+                    "hostBasedRouting": false,
+                    "l3OutRefs": [],
+                    "mac": "00:22:BD:F8:19:FF"
+                }
+            ],
+            "contracts": [],
+            "externalEpgs": [],
+            "serviceGraphs": [],
+            "intersiteL3outs": [],
+            "networks": []
+        }
+    ]
+}
+'''
+
+
 print("Pushing blob")
 resp = rc.post('/schemas', json_body=json.loads(postData))
 print("Associate to site")
 resp = rc.put('/schemas/613927591f0000080a98c007', json_body=json.loads(siteData))
 print("Deploy to site")
+resp = rc.get('/execute/schema/613927591f0000080a98c007/template/testTemplate')
+print(resp.text)
+print("Adding static path binding pod-1, leaf-1, eth1/6, vlan 666")
+resp = rc.put('/schemas/613927591f0000080a98c007', json_body=json.loads(staticPathBinding))
+print("Deploy modified schema to site")
 resp = rc.get('/execute/schema/613927591f0000080a98c007/template/testTemplate')
 print(resp.text)
